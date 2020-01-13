e90e12b67126d2d0484d67025a5a317a671a3d47
steven <steveqiulei@gmail.com>
2020-01-13 15:23:54 +0800

    add design circular queue

---
+++ design_circular_queue.cpp
@@ -0,0 +1,63 @@
+class MyCircularQueue {
+private:
+    private:
+    vector<int> arr;
+    int front;
+    int rear;
+    int capacity;
+public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    MyCircularQueue(int k) {
+        capacity = k + 1;
+        arr.assign(capacity, 0);
+
+        front = 0;
+        rear = 0;
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    bool enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        arr[rear] = value;
+        rear = (rear + 1) % capacity;
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        front = (front + 1) % capacity;
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[front];
+    }
+
+    /** Get the last item from the queue. */
+    int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[(rear - 1 + capacity) % capacity];
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    bool isEmpty() {
+        return front == rear;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    bool isFull() {
+        // 注意：这是这个经典设计的原因
+        return (rear + 1) % capacity == front;
+    }
+};
